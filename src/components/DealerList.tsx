7076430cf0d60a2c08c4de4a84b3379d1f74efe7
Hould222 <vitaly-chubatov@yandex.ru>
2025-01-27 00:55:07 +0300

    стили

--- src/components/DealerList.tsx
+++ src/components/DealerList.tsx
@@ -6,19 +6,15 @@ import { Button } from 'antd';
 
 const DealerList: React.FC<DealerListProps> = ({ dealerList, filterDealers }) => {
     return (
-        <div >
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px'}}>
             <h2>Список дилеров</h2>
-            <div style={{ display: 'flex' , }}>
+            <div style={{ display: 'flex' , gap: '20px', margin: '10px' }}>
                 <Button onClick={() => filterDealers('1и2')}>Дилеры 1 и 2</Button>
                 <Button onClick={() => filterDealers('2и3')}>Дилеры 2 и 3</Button>
                 <Button onClick={() => filterDealers('все')}>Все дилеры</Button>
             
             </div>
-            <ul>
-                {dealerList.map((dealerId) => (
-                    <li key={dealerId}>{dealerId}</li>
-                ))}
-            </ul>
+           
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import '@ant-design/v5-patch-for-react-19';
 import ReactDOM from 'react-dom/client'; 
 import { Provider } from 'react-redux';
 import { store } from './store.ts';
+import './index.css'
 import App from './App.tsx';
 
 
